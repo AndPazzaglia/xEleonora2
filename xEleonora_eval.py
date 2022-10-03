62d25fbe6a1e3b967e14cbc65d8d646274cadf5a
Andrea Pazzaglia <andrea.pazzaglia91@gmail.com>
2022-10-03 18:02:34 +0200

    fine tuning gpt 2

--- xEleonora_eval.py
+++ xEleonora_eval.py
@@ -4,105 +4,12 @@ from tensorflow.keras.models import load_model
 from utils.EvalModel import EvalModel
 
 #%% load model
-model_name = "epoch_35"
+model_name = "gpt2-poetries"
 model_path = os.path.join('models', model_name)
 model = load_model(model_path)
 final_output = EvalModel(
-    model, author_name='ALDA MERINI', input_words='il tuo sorriso', out_lines_number=13, temperature=0.2)
+    model, input_words='il tuo sorriso è come')
 
 print('--------------------------------------------------------')
 print(final_output)
 print('--------------------------------------------------------')
-
-# input_size = 10
-
-# #%% load tokenizer
-    
-# tok_path = os.path.join("tokenization", "tokenizer.pkl")
-# with open(tok_path, 'rb') as f:
-#     tokenizer = pickle.load(f)
-
-# onehot_path = os.path.join("tokenization", "onehotencoder.pkl")
-# with open(onehot_path, 'rb') as f:
-#     onehot = pickle.load(f)
-    
-
-# #%% author choice
-
-# # author_name = 'ALDA MERINI'
-# author_name = 'CAIO VALERIO CATULLO'
-# # author_name = 'DINO BUZZATI'
-# # author_name = 'WILLIAM WORDSWORTH'
-# # author_name = 'EDGAR ALLAN POE'
-# # author_name = 'EUGENIO MONTALE'
-# # author_name = 'GIACOMO LEOPARDI'
-# # author_name = 'GIUSEPPE UNGARETTI'
-# # author_name = 'DANTE ALIGHIERI'
-# # author_name = 'WILLIAM SHAKESPEARE'
-
-# input_aut = onehot.transform(np.array([author_name]).reshape(1, -1))
-
-# #%% model prediction
-    
-# out_lines_number = 14
-# temperature = 0.2
-# input_words = "Il tuo sorriso"
-# input_seq = np.array(tokenizer.texts_to_sequences([input_words]))
-# if len(input_seq[0]) > input_size:
-#     input_seq = input_seq[0][len(input_seq[0])-input_size:]
-#     input_seq = input_seq.reshape(1, input_size)
-# elif len(input_seq[0]) < input_size:
-#     input_seq = np.append(np.zeros(input_size-len(input_seq[0])), input_seq[0])
-#     input_seq = input_seq.reshape(1, input_size)
-
-# def predict_word(input_seq, model, tokenizer, temperature):
-#     preds = model.predict([input_seq, input_aut])
-#     weights = preds**(1/temperature) / np.sum(preds**(1/temperature))
-#     weights = weights.reshape(len(tokenizer.word_index)+1)
-#     word_id = random.choices(population=np.arange(0, 1 + len(tokenizer.word_index)), weights=weights)
-#     return word_id[0]
-
-# lines = 0
-# output_words = input_words
-# for i in range(500):
-#     word_id = predict_word(input_seq, model, tokenizer, temperature)
-#     out_word = tokenizer.index_word[word_id]
-#     if out_word == '\n':
-#         lines = lines + 1
-#         if lines >= out_lines_number:
-#             break
-#     output_words = output_words + ' ' + out_word
-#     tmp = input_seq[0][1:]
-#     input_seq = np.reshape(np.append(tmp, word_id), (1, input_size))
-    
-# final_output = output_words.replace("\n ", "\n").replace(" .", ".").replace(" ,", ",")
-# print('--------------------------------------------------------')
-# print(final_output)
-# print('--------------------------------------------------------')
-    
-# text_number = 0
-# list_dir = os.listdir()
-# for file_name in list_dir:
-#     if "text_" in file_name:
-#         tmp = int(file_name[5:7])
-#         if tmp > text_number:
-#             text_number = tmp
-#%% text to speech
-
-# from gtts import gTTS
-# from playsound import playsound
-# import os
-
-# tts = gTTS(text=final_output.replace(' l ', ' il '), lang='it')
-
-# audio_number = 0
-# list_dir = os.listdir()
-# for file_name in list_dir:
-#     if "audio_" in file_name:
-#         tmp = int(file_name[6:8])
-#         if tmp > audio_number:
-#             audio_number = tmp
-
-# audio_name = 'audio_' + str(audio_number + 1).zfill(2) + '.mp3'
-# tts.save(audio_name)
-# playsound(audio_name)
---
+++ xEleonora_train_from_scratch_lstm.py
@@ -0,0 +1,154 @@
+#%%
+
+import pickle 
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, LSTM, Embedding, GRU, concatenate
+from tensorflow.keras import Input, Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from os import path
+
+#%% load dataset, tokenizer, embeddings matrix
+
+auth_path = os.path.join('tokenization','clean_poetries_authors.pkl')
+with open(auth_path, 'rb') as f:
+    clean_poetries_authors = pickle.load(f)
+
+tok_path = os.path.join('tokenization', 'tokenizer.pkl')
+with open(tok_path, 'rb') as f:
+    tokenizer = pickle.load(f)
+
+emb_path = os.path.join('tokenization', 'embedding_matrix.pkl')
+with open(emb_path, 'rb') as f:
+    embedding_matrix = pickle.load(f)
+
+#%% prepare dataset: add one hot encoding of authors
+
+from sklearn.preprocessing import OneHotEncoder
+
+onehot = OneHotEncoder(sparse=False)
+authors = clean_poetries_authors[0, :]
+authors = authors.reshape(-1, 1)
+authors_onehot = onehot.fit_transform(authors)
+authors_number = len(authors_onehot[0])
+
+onehot_path = os.path.join('tokenization', 'onehotencoder.pkl')
+with open(onehot_path, 'wb') as fp:
+    pickle.dump(onehot, fp)
+
+#%% prepare dataset: convert poetries to sequences
+from sklearn.model_selection import train_test_split
+poetries = clean_poetries_authors[1, :]
+encoded = tokenizer.texts_to_sequences(poetries)
+
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+
+input_size = 16
+X = []
+y = []
+y_emb = []
+for j, seq in enumerate(encoded):
+    aut_oh = authors_onehot[j,:]
+    for i in range(input_size, len(seq)):
+        tmp = np.array(seq[i-input_size:i])
+        tmp = np.concatenate((tmp, aut_oh))
+        X.append(tmp)
+        y.append(seq[i])
+        y_emb.append(embedding_matrix[seq[i]])
+
+X = np.array(X)
+y = np.array(y)
+y_emb = np.array(y_emb)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+X_train, X_test, y_emb_train, y_emb_test = train_test_split(X, y_emb, test_size=0.2, random_state=42)
+
+X_train_seq = X_train[:, 0:input_size]
+X_train_aut = X_train[:, input_size:]
+X_test_seq = X_test[:, 0:input_size]
+X_test_aut = X_test[:, input_size:]
+
+#%% define model new structure - regression of embeddings & authors one hot encoding
+
+input_seq = Input(shape=(input_size,))
+emb = Embedding(vocab_size, 300, weights=[embedding_matrix], input_length=input_size, trainable=False)(input_seq)
+lstm = LSTM(64)(emb)
+
+input_aut = Input(shape=(authors_number,))
+conc = concatenate([lstm, input_aut])
+
+dense1 = Dense(64, activation='tanh')(conc)
+out = Dense(300, activation='linear')(dense1)
+
+model = Model(inputs=[input_seq, input_aut], outputs=out)
+
+print(model.summary())
+
+# compile network
+model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
+
+print(model.summary())
+
+# define checkpoint saving callback
+checkpoint_saving = ModelCheckpoint(
+    'models\epoch_{epoch}', save_freq='epoch',
+)
+
+# fit network
+history = model.fit(
+    [X_train_seq, X_train_aut], y_train, epochs=100, verbose=1, 
+    batch_size=64, validation_data=([X_test_seq, X_test_aut], y_test),
+    callbacks=[checkpoint_saving, TensorBoard()])
+
+
+plt.figure()
+plt.plot(history.history['mse'])
+plt.plot(history.history['val_mse'])
+plt.show()
+
+#%% define model new structure - cross entropy & authors one hot encoding
+
+# input_seq = Input(shape=(input_size,))
+# emb = Embedding(vocab_size, 300, weights=[embedding_matrix], input_length=input_size, trainable=False)(input_seq)
+# # lstm = LSTM(256, return_sequences=True)(emb)
+# # lstm = LSTM(256)(lstm)
+# lstm = LSTM(64)(emb)
+
+
+# input_aut = Input(shape=(authors_number,))
+# conc = concatenate([lstm, input_aut])
+
+# dense1 = Dense(64, activation='relu')(conc)
+# # dense2 = Dense(256, activation='relu')(dense1)
+# # out = Dense(vocab_size, activation='softmax')(dense2)
+# out = Dense(vocab_size, activation='softmax')(dense1)
+
+# model = Model(inputs=[input_seq, input_aut], outputs=out)
+
+# print(model.summary())
+
+# # compile network
+# opt = Adam(learning_rate=0.01)
+# model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['sparse_categorical_accuracy'])
+
+# # define checkpoint saving callback
+# checkpoint_saving = ModelCheckpoint(
+#     'models\epoch_{epoch}', save_freq='epoch',
+# )
+
+# # fit network
+# history = model.fit(
+#     [X_train_seq, X_train_aut], y_train, epochs=100, verbose=1, 
+#     batch_size=64, validation_data=([X_test_seq, X_test_aut], y_test),
+#     callbacks=[checkpoint_saving, TensorBoard()])
+
+
+# plt.figure()
+# plt.plot(history.history['sparse_categorical_accuracy'])
+# plt.plot(history.history['val_sparse_categorical_accuracy'])
+# plt.show()
