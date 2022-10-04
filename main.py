89509fb7761b601fa0e311572ebc545948eabcaf
Andrea Pazzaglia <andrea.pazzaglia91@gmail.com>
2022-10-04 09:28:14 +0200

    update requirements

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.write(
     'xEleonora is an AI model which generates italian poetries through a GPT2 fine tuning.')
 st.sidebar.header('Instructions:')
 st.sidebar.write(
-    'Fill the following inputs and then hit the Run button!')
+    'Load model before using it.')
 
 # choice of the author style
 # auth_path = os.path.join('tokenization','clean_poetries_authors.pkl')
@@ -21,17 +21,21 @@ st.sidebar.write(
 # authors = clean_poetries_authors[0, :]
 # authors = list(dict.fromkeys(authors))
 # selected_author = st.sidebar.selectbox('Select the author style', authors, index=1)
-# text input
-input_words = st.sidebar.text_input('Write initial words of the poetry', value="Il tuo sorriso è come")
+
 # out_lines_number = st.sidebar.slider('Select the number of lines of the poetry', min_value=1, max_value=50, value=13, step=1)
 # temperature = st.sidebar.slider('Select the model temperature (higher leads to more variable results)', min_value=0.0, max_value=1.0, value=0.5, step=0.1)
 
 # load model from splitted model
-with st.spinner('Loading model...'):
-    modelpath = os.path.join("models", "gpt2-poetries", "pytorch_model.bin")
-    bysp.combine_file(modelpath)
-st.success('Done!')
+if st.sidebar.button('Load Model'):
+    with st.spinner('Loading model...'):
+        modelpath = os.path.join("models", "gpt2-poetries", "pytorch_model.bin")
+        bysp.combine_file(modelpath)
+    st.success('Model loaded')
 
+st.sidebar.write(
+    'Fill the following inputs and then hit the Run button!')
+# text input
+input_words = st.sidebar.text_input('Write initial words of the poetry', value="Il tuo sorriso è come")
 if st.sidebar.button('Run model'):
     # load model
     model = "gpt2-poetries"
